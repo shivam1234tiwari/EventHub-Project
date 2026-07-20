afe956a4557028a322b7ad2f445f69066f0ae882
Rahul Tiwari <t.rahul3105@gmail.com>
2026-07-20 23:53:23 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+  return (
+    <>
+      <h2>Rahul Tiwari</h2>
+      <p>MCA Student</p>
+      <p>MERN STACK || Backend Developer</p>
+    </>
+  )
+}
+
+export default App
